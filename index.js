4096b1bd13729db108cc0130d4241b436cee8a75
Tunji Sunmonu <tunji.sunmonu@gmail.com>
2014-08-22 21:34:12 +0300

    jquery file added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var app = require('express')();
-
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(request, response){
 	response.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+  	console.log('message: ', msg);
+  	io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
